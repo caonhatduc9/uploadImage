9df323e23570f777f8700f446a3579c37e4c7af8
CaoDuc <caonhatduc9@gmail.com>
2023-06-26 12:33:07 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-// import { ConfigService } from '@nestjs/config';
-import * as path from 'path';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // await app.listen(7100);
-
-
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-  );
-  // app.useStaticAssets(path.join(__dirname, '..', 'uploads'));
-  // console.log(path.join(__dirname, '..', 'uploads'));
+  const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
-    origin: ['http://localhost:5500', '127.0.0.1:5500', 'http://chuyenhaidua.id.vn'], // add rontend url here
+    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://chuyenhaidua.id.vn'], // add rontend url here
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control',
     credentials: true,
   };
   app.enableCors(corsOptions);
   await app.listen(7100);
-
 }
+
+
 bootstrap();
